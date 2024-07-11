f42f32b0c166a84fe9b2d44a8d953a1f1bac1a13
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-07-11 13:01:04 -0400

    profile image

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,3 @@ export {default as PasswordGenerator} from './projects/PasswordGenerator.png';
 export {default as SocialClub} from './projects/SocialClub.png';
 export {default as PayrollTracker} from './projects/PayrollTracker.png';
 export {default as PersonalBlog} from './projects/PersonalBlog.png';
-
