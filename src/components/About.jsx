6faf978cead2e3b4707d092d0bbfccc7cfdd0987
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-07-11 13:03:37 -0400

    img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-            <img src= '../assets/IconImage.png' style={{ width: '180px', height: '180px', borderRadius: '50%' }} />
+            <img src='../assets/IconImage.png' />
             </div>
               <p>
                   Josie Zobel 
