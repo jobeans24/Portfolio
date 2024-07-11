6a05f4510031da0dc4887705ea27152579f770ba
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-07-11 13:15:12 -0400

    seeing if this way of importing the file works

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,17 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-          <img src={require('../../assets/profile/image.jpg')} alt="Profile" />
-            </div>
-              <p>
-                  Josie Zobel 
-               </p>
-               <p>
-                  I am a Full Stack web developer who recently graduated from George Washington University Coding Bootcamp. I have experience with HTML, CSS, JavaScript, Node.js, Express.js, SQL, and MongoDB. I am excited to continue learning and growing as a developer.
-                </p>
-             </div>
-            </section>
-          );
+          <i className="IconImage" style={{ fontSize: "96px"}}>A little Glimpse of Me</i>
+        </div>
+        <p>
+          Josie Zobel 
+        </p>
+        <p>
+          I am a Full Stack web developer who recently graduated from George Washington University Coding Bootcamp. I have experience with HTML, CSS, JavaScript, Node.js, Express.js, SQL, and MongoDB. I am excited to continue learning and growing as a developer.
+        </p>
+      </div>
+    </section>
+  );
         }
 
         export default About;
